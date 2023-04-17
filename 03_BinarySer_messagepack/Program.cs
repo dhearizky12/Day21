b16c7ee3d2233d4dce5072c77ebdc96aac7717a1
Kinara <91768638+kinarajv@users.noreply.github.com>
2023-04-17 16:21:57 +0000

    Initial commit

---
+++ 03_BinarySer_messagepack/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using MessagePack;
+
+[MessagePackObject]
+public class Person
+{
+	[Key(0)]
+	public string Name { get; set; }
+	[Key(1)]
+	public int Age { get; set; }
+}
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		Person person = new Person { Name = "Alice", Age = 10 };
+
+		// Serialize the object
+		byte[] serializedData = MessagePackSerializer.Serialize(person);
+
+		// Save binary data to a file
+		File.WriteAllBytes("person.bin", serializedData);
+
+		// Read binary data from a file
+		byte[] dataFromFile = File.ReadAllBytes("person.bin");
+
+		// Deserialize the object
+		Person deserializedPerson = MessagePackSerializer.Deserialize<Person>(dataFromFile);
+
+		Console.WriteLine($"Deserialized Person: {deserializedPerson.Name}, {deserializedPerson.Age}");
+	}
+}
